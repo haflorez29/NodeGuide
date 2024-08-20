1438bdaac0675373098d1f9bf6bf20d5b3a2d0c6
heidy <heidyflorez55@gmail.com>
2024-08-19 20:26:40 -0500

    arrow function

--- 02-BASES/src/app.js
+++ 02-BASES/src/app.js
@@ -1,5 +1,14 @@
 // console.log("hola execute");
-const {emailTemplate} = require("./js-foundation/01-template");
+// const {emailTemplate} = require("./js-foundation/01-template");
+// -----------------------------
 // console.log(templateExport);
-require('./js-foundation/02-destructuring')
+// require('./js-foundation/02-destructuring')
+// ------------------------------
+const {getUserById} = require('./js-foundation/03-callbacks')
+// console.log(process.env);
+const id = 3
+getUserById(id, (error, user) =>{
+    if(error) throw new Error(error);
+    console.log(user);
+});
 
