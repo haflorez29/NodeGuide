8c82e49b086ee7bfcec32750c1ad0f98b80947b0
heidy <heidyflorez55@gmail.com>
2024-08-23 18:34:14 -0500

    async await

--- 02-BASES/src/js-foundation/06-promises.js
+++ 02-BASES/src/js-foundation/06-promises.js
@@ -1,11 +1,19 @@
-const getPokemonById = (id,callback) => {
+const getPokemonById = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-    return fetch(url)
-        // .then((response) =>response.json().then(data =>console.log(data)))
-        // .catch()
-        .then((response)=>response.json())
-        .then(()=> { throw new Error('pokemon dont exist')})
-        .then(data =>data.name)
+    // return fetch(url)
+    //     // .then((response) =>response.json().then(data =>console.log(data)))
+    //     // .catch()
+    //     .then((response)=>response.json())
+    //     .then(()=> { throw new Error('pokemon dont exist')})
+    //     .then(data =>data.name)
+    // asyn await
+    try {
+        const resp = await fetch(url)
+        const pokemon = await resp.json()
+        return pokemon.name
+    } catch {
+        console.error('pokemon no existe');
+    }
 }
 
 module.exports = getPokemonById
