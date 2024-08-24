3b83bcc1cbc60064dcbbb10677882bdaef4c2d8f
heidy <heidyflorez55@gmail.com>
2024-08-23 22:56:32 -0500

    axios

--- 02-BASES/src/plugins/http-client.plugin.js
+++ 02-BASES/src/plugins/http-client.plugin.js
@@ -1,10 +1,24 @@
 // adapter of fetchAPI
-const httpClientPlugin = {
-    get: async (url) => {
-        const resp = await fetch(url)
-        return await resp.json()
-    },
+const axios = require('axios');
 
+// idea para el caso en que se necesito apis publicas y una con validación
+// const buildHttpClient = (headers)=>{
+//     return
+// }
+
+const httpClientPlugin = {
+    // fetch
+    // get: async (url) => {
+    //     const resp = await fetch(url)
+    //     return await resp.json()
+    // },
+    
+    // axios
+    get: async (url) => {
+        const { data } = await axios.get(url)
+        return await data
+    },
+    
     post: async (url, body) => {
         const resp = await fetch(url)
         return await resp.json()
